3573c9dd281588fd5679ff73072e7768cd93cdd7
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-08-08 19:07:42 +0300

    Update xml.js

--- xml.js
+++ xml.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = '.'
 
 client.on('ready', () => {
-    client.user.setActivity("You Sexy Ass Girl",{type: 'WATCHING'})
+    client.user.setActivity("You Hahahaha",{type: 'WATCHING'})
 
 });
 
